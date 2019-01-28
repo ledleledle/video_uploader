f6211b74408776dd88c7005c541b2ae6ee7b30fd
Anwareset <anwareset@gmail.com>
2019-01-28 12:21:17 +0700

    ngedit

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', 'Edibawer21');
-if (!$connection){
-	die("Koneksi Database gagal!" . mysqli_error($connection));
-}
-$select_db = mysqli_select_db($connection, 'video_uploader');
-if (!$select_db){
-	die("Koneksi Database gagal!" . mysqli_error($connection));
+
+$host = "localhost";
+$user = "root";
+$password = "Edibawer21";
+$db = "video_upload";
+
+$con = mysqli_connect($host, $user, $password,$db);
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
 }
--- index.php
+++ index.php
@@ -1,4 +1,51 @@
- <form enctype="multipart/form-data" action="upload.php" method="POST">
- Pilih file: <input name="uploaded" type="file" /><br />
- <input type="submit" value="Upload" />
- </form> 
+<!doctype html>
+<html>
+  <head>
+    <?php
+    include("config.php");
+ 
+    if(isset($_POST['but_upload'])){
+       $maxsize = 104857600; // 100MB
+ 
+       $name = $_FILES['file']['name'];
+       $target_dir = "videos/";
+       $target_file = $target_dir . $_FILES["file"]["name"];
+
+       // Select file type
+       $videoFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+       // Valid file extensions
+       $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
+
+       // Check extension
+       if( in_array($videoFileType,$extensions_arr) ){
+ 
+          // Check file size
+          if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
+            echo "File terlalu besar. Maksimum 100MB";
+          }else{
+            // Upload
+            if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
+              // Insert record
+              $query = "INSERT INTO videos(name,location) VALUES('".$name."','".$target_file."')";
+
+              mysqli_query($con,$query);
+              echo "Upload successfully.";
+            }
+          }
+
+       }else{
+          echo "Format file tidak diketarhui";
+       }
+ 
+     } 
+     ?>
+  </head>
+  <body>
+    <form method="post" action="" enctype='multipart/form-data'>
+      <input type='file' name='file' />
+      <input type='submit' value='Upload' name='but_upload'>
+    </form>
+
+  </body>
+</html>
